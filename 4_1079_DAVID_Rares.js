bfc49445d83a05c9cc97103150f6743b8f081e22
RareIsTheVoid <davidr9o@yahoo.ro>
2020-11-16 23:16:20 +0200

    small adjustments to canvas

--- 4_1079_DAVID_Rares.js
+++ 4_1079_DAVID_Rares.js
@@ -1,20 +1,26 @@
 window.onload = function() {
 
     document.getElementById("getStartedButton").onclick = () => {
+        //transition to second screen
         document.getElementById("title").style.fontSize="50px";
         document.getElementById("description").remove()
         var getStartedButton = document.getElementById("getStartedButton");
         getStartedButton.remove();
         document.getElementById("authorLink").remove();
 
+
+        //creating canvas
         var canvas = document.createElement("Canvas");
         var ctx = canvas.getContext("2d");
-        canvas.height=450;
-        canvas.width=1200;
+        canvas.height=480;
+        canvas.width=720;
         canvas.style.border="3px solid #540a0a";
+        canvas.style.marginTop="5px";
         ctx.fillStyle="white"
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         document.body.append(canvas);
+
+
     }
 
 }
