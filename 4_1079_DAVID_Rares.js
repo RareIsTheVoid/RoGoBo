3bbf731c0c2354f4f37e23efb2b7f27cf99dfce8
Rares David <david_rares@yahoo.com>
2020-12-22 19:34:08 +0200

    added load feature

--- 4_1079_DAVID_Rares.js
+++ 4_1079_DAVID_Rares.js
@@ -1,27 +1,47 @@
 window.onload = function() {
 
+    //setting up canvas
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+
     document.getElementById("getStartedButton").onclick = () => {
+
         //transition to second screen
         document.getElementById("title").style.fontSize="50px";
         document.getElementById("description").remove()
-        var getStartedButton = document.getElementById("getStartedButton");
+        let getStartedButton = document.getElementById("getStartedButton");
         getStartedButton.remove();
         document.getElementById("authorLink").remove();
+        document.getElementById("canvasContainer").style.display="block";
+        document.getElementById("editButtons").style.display="flex";
+        document.getElementById("editButtons").style.flexDirection="row";
 
-
-        //creating canvas
-        var canvas = document.createElement("Canvas");
-        var ctx = canvas.getContext("2d");
-        canvas.height=480;
-        canvas.width=720;
-        canvas.style.border="3px solid #540a0a";
-        canvas.style.marginTop="5px";
         ctx.fillStyle="white"
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        document.body.append(canvas);
-
-
     }
 
+    input.onchange = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle="white"
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        let image = new Image();
+        image.onload = function() {
+
+            let ratio = image.width / image.height;
+            let newWidth = canvas.width;
+            let newHeight = newWidth / ratio;
+            if (newHeight > canvas.height) {
+                newHeight = canvas.height;
+                newWidth = newHeight * ratio;
+            }
+
+            let positionX = (canvas.width - newWidth)/2;
+            let positionY = (canvas.height - newHeight)/2;
+
+            ctx.drawImage(image, positionX, positionY, newWidth, newHeight);
+        }
+        image.src=URL.createObjectURL(this.files[0]);
+    }
 }
 
